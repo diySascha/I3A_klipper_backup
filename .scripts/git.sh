62b81c3941a9080dc47ab48ce439de7dc5e3c328
diySascha <UncleSash@gmx.li>
2023-01-24 19:35:04 +0100

    Backup_2023_01_24

--- .scripts/git.sh
+++ .scripts/git.sh
@@ -3,10 +3,12 @@ file_date=(`date +%Y-%m-%d_%H-%M`)
 log_date=(`date +%Y-%m-%d_%H:%M:%S`)
 echo $log_date
 echo "-------------------------------------------------" >> /home/pi/printer_data/config/Backuplogs/git_ready.log
-echo $log_date >> /home/pi/printer_data/config/Backuplogs/log_ready.log
+echo $log_date >> /home/pi/printer_data/config/Backuplogs/git_ready.log
+echo "START--------------------------------------------" >> /home/pi/printer_data/config/Backuplogs/git_ready.log
 echo "-------------------------------------------------" >> /home/pi/printer_data/config/Backuplogs/git_fail.log
-echo $log_date >> /home/pi/printer_data/config/Backuplogs/log_fail.log
-cd ~/printer_data/config 1>> /home/pi/printer_data/config/Backuplogs/git_ready.log 2>> /home/pi/printer_data/config/Backuplogs/git_fail.log
+echo $log_date >> /home/pi/printer_data/config/Backuplogs/git_fail.log
+echo "START--------------------------------------------" >> /home/pi/printer_data/config/Backuplogs/git_fail.log
+cd ~/printer_data/config >> /home/pi/printer_data/config/Backuplogs/git_ready.log
 sleep 1s ; git remote -v 1>> /home/pi/printer_data/config/Backuplogs/git_ready.log 2>> /home/pi/printer_data/config/Backuplogs/git_fail.log
 sleep 1s ; git remote rm origin 1>> /home/pi/printer_data/config/Backuplogs/git_ready.log 2>> /home/pi/printer_data/config/Backuplogs/git_fail.log
 sleep 1s ; git remote add origin https://github.com/diySascha/I3A_klipper_backup.git 1>> /home/pi/printer_data/config/Backuplogs/git_ready.log 2>> /home/pi/printer_data/config/Backuplogs/git_fail.log
@@ -16,4 +18,11 @@ sleep 1s ; git add . 1>> /home/pi/printer_data/config/Backuplogs/git_ready.log 2
 sleep 1s ; git commit -m Backup_`date +%Y_%m_%d` 1>> /home/pi/printer_data/config/Backuplogs/git_ready.log 2>> /home/pi/printer_data/config/Backuplogs/git_fail.log
 sleep 1s ; git branch -M main 1>> /home/pi/printer_data/config/Backuplogs/git_ready.log 2>> /home/pi/printer_data/config/Backuplogs/git_fail.log
 sleep 1s ; git push -f origin main 1>> /home/pi/printer_data/config/Backuplogs/git_ready.log 2>> /home/pi/printer_data/config/Backuplogs/git_fail.log
-
+log_date=(`date +%Y-%m-%d_%H:%M:%S`)
+echo $log_date
+echo "-------------------------------------------------" >> /home/pi/printer_data/config/Backuplogs/git_ready.log
+echo $log_date >> /home/pi/printer_data/config/Backuplogs/git_ready.log
+echo "DONE---------------------------------------------" >> /home/pi/printer_data/config/Backuplogs/git_ready.log
+echo "-------------------------------------------------" >> /home/pi/printer_data/config/Backuplogs/git_fail.log
+echo $log_date >> /home/pi/printer_data/config/Backuplogs/git_fail.log
+echo "DONE---------------------------------------------" >> /home/pi/printer_data/config/Backuplogs/git_fail.log
